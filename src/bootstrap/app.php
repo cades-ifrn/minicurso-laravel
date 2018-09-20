502ca18690c95c45333e178967890e7cdfc01aed
Yves Cabral <yvescabral16@gmail.com>
2018-09-20 14:20:57 -0300

    complete code

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -59,9 +59,9 @@ $app->singleton(
 |
 */
 
-// $app->middleware([
-//    App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    Vluzrmos\LumenCors\CorsMiddleware::class
+]);
 
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
